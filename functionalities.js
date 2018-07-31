83e4b1f9c800cc7cabee0d66b292016c9428ea02
Piotr Szubert <piotr.szubert1@wp.pl>
2018-07-31 14:06:35 +0200

    matchMedia on buttons

--- functionalities.js
+++ functionalities.js
@@ -5,7 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
     var menu = document.querySelectorAll(".header__li");
 
     var img = document.querySelectorAll(".gallery__img");
-    
+
+    menu[0].addEventListener("mouseover", function (event) {
+        submenu.classList.add("show");
+    });
+
+    submenu.addEventListener("mouseout", function (event) {
+        submenu.classList.remove("show");
+    });
 
     menu[0].addEventListener("touchstart", function (event) {
         submenu.style.display= "block";
@@ -22,6 +29,19 @@ document.addEventListener("DOMContentLoaded", function () {
             underline.classList.add("disappearing");
         });
         img[i].addEventListener("mouseout", function (event) {
+            var underline = this.querySelector(".gallery__underline");
+            underline.classList.remove("disappearing");
+            underline.classList.add("appearing");
+        })
+    }
+
+    for(var i = 0;i<img.length;i++) {
+        img[i].addEventListener("touchstart", function (event) {
+            var underline = this.querySelector(".gallery__underline");
+            underline.classList.remove("appearing");
+            underline.classList.add("disappearing");
+        });
+        img[i].addEventListener("touchend", function (event) {
             var underline = this.querySelector(".gallery__underline");
             underline.classList.remove("disappearing");
             underline.classList.add("appearing");
@@ -61,6 +81,18 @@ document.addEventListener("DOMContentLoaded", function () {
         this.firstElementChild.classList.add("main__sidebutton--effectOut");
     });
 
+    buttons[0].addEventListener("touchstart", function (event) {
+        event.preventDefault();
+        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
+        this.firstElementChild.classList.add("main__sidebutton--effectIn");
+    });
+
+    buttons[0].addEventListener("touchend", function (event) {
+        event.preventDefault();
+        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
+        this.firstElementChild.classList.add("main__sidebutton--effectOut");
+    });
+
     buttons[1].addEventListener("click", function (event) {
         event.preventDefault();
         this.firstElementChild.classList.remove("main__sidebutton--effect");
@@ -88,17 +120,27 @@ document.addEventListener("DOMContentLoaded", function () {
         this.firstElementChild.classList.add("main__sidebutton--effectOut");
     });
 
+    buttons[1].addEventListener("touchstart", function (event) {
+        event.preventDefault();
+        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
+        this.firstElementChild.classList.add("main__sidebutton--effectIn");
+    });
+
+    buttons[1].addEventListener("touchend", function (event) {
+        event.preventDefault();
+        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
+        this.firstElementChild.classList.add("main__sidebutton--effectOut");
+    });
+
     var mobile = window.matchMedia("screen and (max-width: 1023px)");
 
     var changeBtn = document.querySelectorAll(".section__post .container .row>div");
 
     var row =document.querySelector(".section__post .container .row");
 
-    console.log(changeBtn[3]);
-    console.log(row.children[2]);
+    var body = document.querySelector("body");
 
     mobile.addListener( function(mobile) {
-        console.log(changeBtn[3]);
         if (mobile.matches) {
             row.insertBefore(changeBtn[3], row.children[2]);
         }else{
@@ -106,4 +148,10 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    window.onload=function (event) {
+        console.log(window.innerWidth);
+            if(window.innerWidth<1024){
+                row.insertBefore(changeBtn[3], row.children[2]);
+            }
+    };
 });
