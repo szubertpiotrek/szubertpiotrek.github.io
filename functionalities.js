a038b7c271ac3d76aaae3583ea7308cf79843556
Piotr Szubert <piotr.szubert1@wp.pl>
2018-08-04 00:36:38 +0200

    add some effects

--- functionalities.js
+++ functionalities.js
@@ -4,8 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var menu = document.querySelectorAll(".header__li");
 
-    var btn = menu[0].firstElementChild;
-
     var img = document.querySelectorAll(".gallery__img");
 
     var mobileMedium = window.matchMedia("screen and (max-width: 1023px) and (min-width: 641px)");
@@ -18,6 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var row = document.querySelector(".section__post .container .row");
 
+    var costsBoxes = document.querySelectorAll(".costs__box");
+
+    var mainColor = "#27C7AB";
+    
+    var mainButton = document.querySelector(".main__button");
+
+    var costsButton = document.querySelectorAll(".costs__button");
+
+    var submitButton = document.querySelector(".contact__submited");
+
+    var footerImg = document.querySelectorAll(".footer__img--item");
 
 
     menu[0].addEventListener("mouseover", function (event) {
@@ -37,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault();
         submenu.classList.toggle("show");
     });
-    
+
 
     for (var i = 0; i < img.length; i++) {
         img[i].addEventListener("mouseover", function (event) {
@@ -75,203 +84,104 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     images[elementNumber].classList.add("visible");
-    console.log(elementNumber);
 
-    buttons[0].addEventListener("click", function (event) {
-        event.preventDefault();
-        images[elementNumber].classList.remove("visible");
-        elementNumber--;
+    for(var i =0;i<buttons.length;i++){
+        buttons[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            images[elementNumber].classList.remove("visible");
+            elementNumber--;
 
-        if (elementNumber === -1) {
-            elementNumber = images.length - 1;
-        }
+            if (elementNumber === -1) {
+                elementNumber = images.length - 1;
+            }
 
-        images[elementNumber].classList.add("visible");
-    });
+            images[elementNumber].classList.add("visible");
+        });
 
-    buttons[1].addEventListener("click", function (event) {
-        event.preventDefault();
-        images[elementNumber].classList.remove("visible");
-        elementNumber++;
+        buttons[i].addEventListener("touchstart", function (event) {
+            event.preventDefault();
+            images[elementNumber].classList.remove("visible");
+            elementNumber--;
 
-        if (elementNumber >= images.length) {
-            elementNumber = 0;
-        }
+            if (elementNumber === -1) {
+                elementNumber = images.length - 1;
+            }
 
-        images[elementNumber].classList.add("visible");
+            images[elementNumber].classList.add("visible");
+        });
 
-    });
+        insideButtons[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            images[elementNumber].classList.remove("visible");
+            elementNumber--;
 
-    buttons[0].addEventListener("touchstart", function (event) {
-        event.preventDefault();
-        images[elementNumber].classList.remove("visible");
-        elementNumber--;
+            if (elementNumber === -1) {
+                elementNumber = images.length - 1;
+            }
 
-        if (elementNumber === -1) {
-            elementNumber = images.length - 1;
-        }
+            images[elementNumber].classList.add("visible");
+        });
 
-        images[elementNumber].classList.add("visible");
-    });
+        insideButtons[i].addEventListener("touchstart", function (event) {
+            event.preventDefault();
+            images[elementNumber].classList.remove("visible");
+            elementNumber--;
 
-    buttons[1].addEventListener("touchstart", function (event) {
-        event.preventDefault();
-        images[elementNumber].classList.remove("visible");
-        elementNumber++;
+            if (elementNumber === -1) {
+                elementNumber = images.length - 1;
+            }
 
-        if (elementNumber >= images.length) {
-            elementNumber = 0;
-        }
+            images[elementNumber].classList.add("visible");
+        });
 
-        images[elementNumber].classList.add("visible");
+        buttons[i].addEventListener("mouseover", function (event) {
+            event.preventDefault();
+            this.firstElementChild.classList.remove("main__sidebutton--effectOut");
+            this.firstElementChild.classList.add("main__sidebutton--effectIn");
+        });
 
-    });
+        buttons[i].addEventListener("mouseout", function (event) {
+            event.preventDefault();
+            this.firstElementChild.classList.remove("main__sidebutton--effectIn");
+            this.firstElementChild.classList.add("main__sidebutton--effectOut");
+        });
 
+        insideButtons[i].addEventListener("mouseover", function (event) {
+            event.preventDefault();
+            this.firstElementChild.classList.remove("main__sidebutton--effectOut");
+            this.firstElementChild.classList.add("main__sidebutton--effectIn");
+        });
 
+        insideButtons[i].addEventListener("mouseout", function (event) {
+            event.preventDefault();
+            this.firstElementChild.classList.remove("main__sidebutton--effectIn");
+            this.firstElementChild.classList.add("main__sidebutton--effectOut");
+        });
 
-    insideButtons[0].addEventListener("click", function (event) {
-        event.preventDefault();
-        images[elementNumber].classList.remove("visible");
-        elementNumber--;
+        buttons[i].addEventListener("touchstart", function (event) {
+            event.preventDefault();
+            this.firstElementChild.classList.remove("main__sidebutton--effectOut");
+            this.firstElementChild.classList.add("main__sidebutton--effectIn");
+        });
 
-        if (elementNumber === -1) {
-            elementNumber = images.length - 1;
-        }
+        buttons[i].addEventListener("touchend", function (event) {
+            event.preventDefault();
+            this.firstElementChild.classList.remove("main__sidebutton--effectIn");
+            this.firstElementChild.classList.add("main__sidebutton--effectOut");
+        });
 
-        images[elementNumber].classList.add("visible");
-    });
+        insideButtons[i].addEventListener("touchstart", function (event) {
+            event.preventDefault();
+            this.firstElementChild.classList.remove("main__sidebutton--effectOut");
+            this.firstElementChild.classList.add("main__sidebutton--effectIn");
+        });
 
-    insideButtons[1].addEventListener("click", function (event) {
-        event.preventDefault();
-        images[elementNumber].classList.remove("visible");
-        elementNumber++;
-
-        if (elementNumber >= images.length) {
-            elementNumber = 0;
-        }
-
-        images[elementNumber].classList.add("visible");
-    });
-
-    insideButtons[0].addEventListener("touchstart", function (event) {
-        event.preventDefault();
-        images[elementNumber].classList.remove("visible");
-        elementNumber--;
-
-        if (elementNumber === -1) {
-            elementNumber = images.length - 1;
-        }
-
-        images[elementNumber].classList.add("visible");
-    });
-
-    insideButtons[1].addEventListener("touchstart", function (event) {
-        event.preventDefault();
-        images[elementNumber].classList.remove("visible");
-        elementNumber++;
-
-        if (elementNumber >= images.length) {
-            elementNumber = 0;
-        }
-
-        images[elementNumber].classList.add("visible");
-    });
-
-    buttons[0].addEventListener("mouseover", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
-        this.firstElementChild.classList.add("main__sidebutton--effectIn");
-    });
-
-    buttons[0].addEventListener("mouseout", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
-        this.firstElementChild.classList.add("main__sidebutton--effectOut");
-    });
-
-    insideButtons[0].addEventListener("mouseover", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
-        this.firstElementChild.classList.add("main__sidebutton--effectIn");
-    });
-
-    insideButtons[0].addEventListener("mouseout", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
-        this.firstElementChild.classList.add("main__sidebutton--effectOut");
-    });
-
-    buttons[0].addEventListener("touchstart", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
-        this.firstElementChild.classList.add("main__sidebutton--effectIn");
-    });
-
-    buttons[0].addEventListener("touchend", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
-        this.firstElementChild.classList.add("main__sidebutton--effectOut");
-    });
-
-    insideButtons[0].addEventListener("touchstart", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
-        this.firstElementChild.classList.add("main__sidebutton--effectIn");
-    });
-
-    insideButtons[0].addEventListener("touchend", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
-        this.firstElementChild.classList.add("main__sidebutton--effectOut");
-    });
-
-    buttons[1].addEventListener("mouseover", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
-        this.firstElementChild.classList.add("main__sidebutton--effectIn");
-    });
-
-    buttons[1].addEventListener("mouseout", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
-        this.firstElementChild.classList.add("main__sidebutton--effectOut");
-    });
-
-    insideButtons[1].addEventListener("mouseover", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
-        this.firstElementChild.classList.add("main__sidebutton--effectIn");
-    });
-
-    insideButtons[1].addEventListener("mouseout", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
-        this.firstElementChild.classList.add("main__sidebutton--effectOut");
-    });
-
-    buttons[1].addEventListener("touchstart", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
-        this.firstElementChild.classList.add("main__sidebutton--effectIn");
-    });
-
-    buttons[1].addEventListener("touchend", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
-        this.firstElementChild.classList.add("main__sidebutton--effectOut");
-    });
-
-    insideButtons[1].addEventListener("touchstart", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectOut");
-        this.firstElementChild.classList.add("main__sidebutton--effectIn");
-    });
-
-    insideButtons[1].addEventListener("touchend", function (event) {
-        event.preventDefault();
-        this.firstElementChild.classList.remove("main__sidebutton--effectIn");
-        this.firstElementChild.classList.add("main__sidebutton--effectOut");
-    });
+        insideButtons[i].addEventListener("touchend", function (event) {
+            event.preventDefault();
+            this.firstElementChild.classList.remove("main__sidebutton--effectIn");
+            this.firstElementChild.classList.add("main__sidebutton--effectOut");
+        });
+    }
 
     mobileMedium.addListener(function (mobile) {
         if (mobile.matches) {
@@ -321,4 +231,201 @@ document.addEventListener("DOMContentLoaded", function () {
             row.children[1].firstElementChild.lastElementChild.classList.remove("show1");
         }
     };
+
+    for(var i=0;i<costsBoxes.length;i++){
+        costsBoxes[i].addEventListener("mouseover", function (event) {
+            this.style.backgroundColor = mainColor;
+            this.style.border = "1px solid" + mainColor;
+            this.children[0].style.border = "1px solid" + mainColor;
+            this.children[1].style.backgroundColor = mainColor;
+            this.children[2].style.color = "white";
+            this.children[3].style.color = mainColor;
+            this.lastElementChild.style.backgroundColor = "white";
+            this.lastElementChild.children[0].style.color = mainColor;
+        })
+    }
+
+    for(var i=0;i<costsBoxes.length;i++){
+        costsBoxes[i].addEventListener("mouseout", function (event) {
+            this.style.backgroundColor = "#F9F9F9";
+            this.style.border = "1px solid #C2C2C2";
+            this.children[0].style.border = "1px solid #C2C2C2";
+            this.children[1].style.backgroundColor = "#C2C2C2";
+            this.children[2].style.color = mainColor;
+            this.children[3].style.color = "#C2C2C2";
+            this.lastElementChild.style.backgroundColor = mainColor;
+            this.lastElementChild.children[0].style.color = "white";
+        })
+    }
+
+    for(var i=0;i<costsBoxes.length;i++){
+        costsBoxes[i].addEventListener("touchstart", function (event) {
+            this.style.backgroundColor = mainColor;
+            this.style.border = "1px solid" + mainColor;
+            this.children[0].style.border = "1px solid" + mainColor;
+            this.children[1].style.backgroundColor = mainColor;
+            this.children[2].style.color = "white";
+            this.children[3].style.color = mainColor;
+            this.lastElementChild.style.backgroundColor = "white";
+            this.lastElementChild.children[0].style.color = mainColor;
+        })
+    }
+
+    for(var i=0;i<costsBoxes.length;i++){
+        costsBoxes[i].addEventListener("touchend", function (event) {
+            this.style.backgroundColor = "#F9F9F9";
+            this.style.border = "1px solid #C2C2C2";
+            this.children[0].style.border = "1px solid #C2C2C2";
+            this.children[1].style.backgroundColor = "#C2C2C2";
+            this.children[2].style.color = mainColor;
+            this.children[3].style.color = "#C2C2C2";
+            this.lastElementChild.style.backgroundColor = mainColor;
+            this.lastElementChild.children[0].style.color = "white";
+        })
+    }
+    
+    mainButton.addEventListener("mouseover", function (event) {
+        this.classList.add("button__effect");
+        console.log(mainButton);
+    });
+
+    mainButton.addEventListener("mouseout", function (event) {
+        this.classList.remove("button__effect");
+        console.log(mainButton);
+    });
+
+    mainButton.addEventListener("touchstart", function (event) {
+        this.classList.add("button__effect");
+        console.log(mainButton);
+    });
+
+    mainButton.addEventListener("touchend", function (event) {
+        this.classList.remove("button__effect");
+        console.log(mainButton);
+    });
+
+    for(var i=0;i<costsButton.length;i++){
+        costsButton[i].addEventListener("mouseover", function (event) {
+            this.classList.add("button__effect");
+            console.log(mainButton);
+        });
+
+        costsButton[i].addEventListener("mouseout", function (event) {
+            this.classList.remove("button__effect");
+            console.log(mainButton);
+        });
+    }
+
+    for(var i=0;i<costsButton.length;i++){
+        costsButton[i].addEventListener("touchstart", function (event) {
+            this.classList.add("button__effect");
+            console.log(mainButton);
+        });
+
+        costsButton[i].addEventListener("touchend", function (event) {
+            this.classList.remove("button__effect");
+            console.log(mainButton);
+        });
+    }
+
+    submitButton.addEventListener("mouseover", function (event) {
+        this.classList.add("button__effect");
+        console.log(mainButton);
+    });
+
+    submitButton.addEventListener("mouseout", function (event) {
+        this.classList.remove("button__effect");
+        console.log(mainButton);
+    });
+
+    submitButton.addEventListener("touchstart", function (event) {
+        this.classList.add("button__effect");
+        console.log(mainButton);
+    });
+
+    submitButton.addEventListener("touchend", function (event) {
+        this.classList.remove("button__effect");
+        console.log(mainButton);
+    });
+
+    footerImg[0].addEventListener("mouseover", function (event) {
+        this.classList.add("pulse1");
+        console.log(mainButton);
+    });
+
+    footerImg[0].addEventListener("mouseout", function (event) {
+        this.classList.remove("pulse1");
+        console.log(mainButton);
+    });
+
+    footerImg[0].addEventListener("touchstart", function (event) {
+        this.classList.add("pulse1");
+        console.log(mainButton);
+    });
+
+    footerImg[0].addEventListener("touchend", function (event) {
+        this.classList.remove("pulse1");
+        console.log(mainButton);
+    });
+
+    footerImg[1].addEventListener("mouseover", function (event) {
+        this.classList.add("pulse2");
+        console.log(mainButton);
+    });
+
+    footerImg[1].addEventListener("mouseout", function (event) {
+        this.classList.remove("pulse2");
+        console.log(mainButton);
+    });
+
+    footerImg[1].addEventListener("touchstart", function (event) {
+        this.classList.add("pulse2");
+        console.log(mainButton);
+    });
+
+    footerImg[1].addEventListener("touchend", function (event) {
+        this.classList.remove("pulse2");
+        console.log(mainButton);
+    });
+
+    footerImg[2].addEventListener("mouseover", function (event) {
+        this.classList.add("pulse3");
+        console.log(mainButton);
+    });
+
+    footerImg[2].addEventListener("mouseout", function (event) {
+        this.classList.remove("pulse3");
+        console.log(mainButton);
+    });
+
+    footerImg[2].addEventListener("touchstart", function (event) {
+        this.classList.add("pulse3");
+        console.log(mainButton);
+    });
+
+    footerImg[2].addEventListener("touchend", function (event) {
+        this.classList.remove("pulse3");
+        console.log(mainButton);
+    });
+
+    footerImg[3].addEventListener("mouseover", function (event) {
+        this.classList.add("pulse2");
+        console.log(mainButton);
+    });
+
+    footerImg[3].addEventListener("mouseout", function (event) {
+        this.classList.remove("pulse2");
+        console.log(mainButton);
+    });
+
+    footerImg[3].addEventListener("touchstart", function (event) {
+        this.classList.add("pulse2");
+        console.log(mainButton);
+    });
+
+    footerImg[3].addEventListener("touchend", function (event) {
+        this.classList.remove("pulse2");
+        console.log(mainButton);
+    });
+
 });
